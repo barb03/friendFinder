dca0b5649e2510050766003da38ec5b3be246e84
Vader55 <38086201+barb03@users.noreply.github.com>
2018-12-03 08:51:53 -0600

    added js files and html files

---
+++ app/data/friends.js
@@ -0,0 +1,72 @@
+var friends = [
+    {
+      name: 'Barb',
+      profilePic: "../images/barb.jpg",
+      scores: [
+          "4",
+          "5",
+          "5",
+          "2",
+          "4",
+          "5",
+          "4",
+          "4",
+          "3",
+          "4"
+        ]
+    },
+    {
+        name:"Donald Duck",
+        photo:"../images/donald.jpg",
+        scores:[            
+            '5',
+            '1',
+            '4',
+            '4',
+            '5',
+            '1',
+            '2',
+            '5',
+            '4',
+            '1'
+        ]
+    }, 
+    {
+        name:"Mickey Mouse",
+        photo:"../images/mickey.jpg",
+        scores:[            
+            '4',
+            '3',
+            '4',
+            '1',
+            '5',
+            '2',
+            '3',
+            '5',
+            '4',
+            '1'
+        ]
+    }, 
+    {
+        name:"Goofy",
+        photo:"../images/Goofy.gif",
+        scores:[            
+            '4',
+            '4',
+            '2',
+            '3',
+            '2',
+            '2',
+            '3',
+            '5',
+            '2',
+            '1'
+        ]
+    }
+
+
+  ]
+  
+  module.exports = friends;
+
+  
