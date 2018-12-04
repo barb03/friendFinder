dba7ac53d18b1acbf7a5da39979425fc8e7c4c38
Vader55 <38086201+barb03@users.noreply.github.com>
2018-12-03 18:13:12 -0600

    changed parse json code

--- server.js
+++ server.js
@@ -5,12 +5,8 @@ var bodyParser = require("body-parser");
 
 var PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.use(bodyParser.json({type: "application/*+json"}))
-app.use(bodyParser.raw({type: "application/vnd.custom-type"}))
-
-app.use(bodyParser.text({type: "text/html"}))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //added for images
 app.use(express.static(path.join("app","public")));
